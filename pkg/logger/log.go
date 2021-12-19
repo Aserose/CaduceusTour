89b472cb9da8c372961c4652c1130f5f6c217b82
Aserose <shishi-rendan@yandex.ru>
2021-12-19 11:41:15 +0300

    update

--- pkg/logger/log.go
+++ pkg/logger/log.go
@@ -13,25 +13,25 @@ func NewLogger() *logger {
 }
 
 func (l *logger) Info(args ...interface{}) {
-	l.log.Info(args)
+	l.log.Info(args...)
 }
 
 func (l *logger) Infof(format string, args ...interface{}) {
-	l.log.Infof(format, args)
+	l.log.Infof(format, args...)
 }
 
 func (l *logger) Errorf(format string, args ...interface{}) {
-	l.log.Errorf(format, args)
+	l.log.Errorf(format, args...)
 }
 
 func (l *logger) Error(args ...interface{}) {
-	l.log.Error(args)
+	l.log.Error(args...)
 }
 
 func (l *logger) Print(args ...interface{}) {
-	l.log.Print(args)
+	l.log.Print(args...)
 }
 
 func (l *logger) Printf(format string, args ...interface{}) {
-	l.log.Printf(format, args)
+	l.log.Printf(format, args...)
 }
